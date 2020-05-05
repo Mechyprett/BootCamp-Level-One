f080d8b9582e30e49cacc92007ac873ae1ef91d6
Uris <urisnicolas@gmail.com>
2020-05-05 06:22:59 -0800

    creating objects

--- 14/objet.js
+++ 14/objet.js
@@ -39,5 +39,46 @@ someObject.str //doesn't look for "name"
 someObjet[str] //does evaluate str and looks for "name"
 
 //you cannot use dot notation for property names with spaces
-SomeObject.fav color //INVALID
+//   SomeObject.fav color //INVALID
 someObjet["fav color "]; //VALID
+
+//UPDATING DATA
+/*Just like an array: access a property nad reassign it*/
+
+var person ={
+    name:"Travis",
+    age:21,
+    city: "LA"
+};
+
+//TO UPDATE AGE
+person["age"] += 1;
+//TO UPDATE CITY
+person.city ="London";
+
+/*CREATING OBJECTS*/
+//Like arrays, there are a few methods of initializing objects
+
+//make an empty object and then add to it
+
+var person = {}
+person.name ="Travis";
+person.age =21;
+person.city = "LA";
+
+//All at once
+var person ={
+    name:"Travis",
+    age:21,
+    city:"LA"
+};
+
+
+//another way of initializing an object
+var person = new object();
+person.name ="Travis";
+person.age = 21;
+person.city = "LA";
+
+
+
