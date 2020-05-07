94b91ac713c4a0baf9fe136383d80d2259a2f5c0
Uris <urisnicolas@gmail.com>
2020-05-06 23:20:11 -0800

    creando un objeto dentro de un array

--- 14/exercite.js
+++ 14/exercite.js
@@ -1,26 +1,41 @@
 var movieDB  = [
     {
         title:"In Bruges",
+        hasWatched: true,
         rating:5
 
     },
     {
         title:"Frozen",
+        hasWatched:false,
         rating:4.5
 
     },
     {
         title: "Mad Max Fury Road",
+        hasWatched:true,
         rating: 5
 
     },
     {
         title:"Les Miserables",
+        hasWatched:false,
         rating:3.5
 
     },
 
 
 ]
-var hasWatched = ["Watched","not seen","seen"]
- console.log(movieDB )
+
+movieDB.forEach(function (movie) {
+    var result = "You have ";
+    if(movie.hasWatched){
+        result += "watched ";
+    } else {
+        result += "not seen";
+    }
+    result += "\"" + movie.title + "\" -";
+    result += movie.rating +" stars";
+    console.log(result)
+
+})
